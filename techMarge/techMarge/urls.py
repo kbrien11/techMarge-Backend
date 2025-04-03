37f38aca08af24e9d4d08bc518eca78b0054f172
kbrien11 <kbrien11@gmail.com>
2025-04-03 18:45:49 -0400

    added swagger configuartion

--- techMarge/techMarge/urls.py
+++ techMarge/techMarge/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Api documentation",
+        default_version="v1",
+        description="API Docs for my project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("Documentation.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="Schema-swagger-ui",
+    ),
 ]
